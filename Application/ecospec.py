973d9a02765b283ee387697089d5fbc7ec178921
xLahar <84552704+xLahar@users.noreply.github.com>
2026-04-19 23:32:41 -0400

    Change frameless setting to False in ecospec.py

--- Application/ecospec.py
+++ Application/ecospec.py
@@ -176,7 +176,7 @@ def main():
         height=600,
         resizable=False,
         fullscreen=False,
-        frameless=True,
+        frameless=False,
         min_size=(1024, 600),
         background_color='#0a0e14',
         text_select=False,
