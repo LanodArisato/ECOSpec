ff713000707157579e6aea348f28f447bd3783c4
LanodArisato <123771043+LanodArisato@users.noreply.github.com>
2026-04-23 01:44:51 -0400

    added alternative baseline correction

--- Application/processing/processing.py
+++ Application/processing/processing.py
@@ -12,6 +12,8 @@ PROCESSED_DIR = Path(__file__).parent / "spectra/processed"
 LIB_DIR = Path(__file__).parent / "spectra/lib"
 DARK_FILE = Path(__file__).parent / "spectra/lib" / "dark.csv"
 
+LASER_WAVELENGTH = 785  # nm
+
 def process_spectrum(filename, log_callback=print):
     if log_callback:
         log_callback(f"Processing {filename}...")
@@ -36,26 +38,49 @@ def process_spectrum(filename, log_callback=print):
         intensity = intensity - dark_intensity
 
 
-    # --- Wavelength -> wavenumber (cm^-1) ---
-    wavenumber = 1e7 / wavelength
+    # --- Wavelength ->Raman Shift (cm^-1) ---
+    raman_shift = (1/LASER_WAVELENGTH - 1/wavelength) * 1e7
 
     # Sort result
-    sort_idx = np.argsort(wavenumber)
-    wavenumber = wavenumber[sort_idx]
+    sort_idx = np.argsort(raman_shift)
+    raman_shift = raman_shift[sort_idx]    
     intensity = intensity[sort_idx]
 
     # --- Interpolate ---
     x = np.arange(200, 3401, 1)
-    y_interp = np.interp(x, wavenumber, intensity)
+    y_interp = np.interp(x, raman_shift, intensity)
 
-    # --- Processing pipeline ---
+    # --- Median Filter ---
     df_proc = pd.DataFrame({"WAVENUMBER": x, "INTENSITY_RAW": y_interp})
-
     df_proc["INTENSITY_MED"] = medfilt(df_proc["INTENSITY_RAW"], kernel_size=15)
 
+    # --- Baseline Correction (polynomial fit)---
     p = Polynomial.fit(df_proc["WAVENUMBER"], df_proc["INTENSITY_MED"], deg=7)
     baseline = p(df_proc["WAVENUMBER"])
     df_proc["INTENSITY_CORR"] = df_proc["INTENSITY_MED"] - baseline
+
+    # --- ALS baseline (alternative) ---
+    # y_med = df_proc["INTENSITY_MED"].values
+    
+    # lam = 1e5
+    # asym = 0.01
+    # niter = 10
+
+    # L = len(y_med)
+    # D = np.diff(np.eye(L), 2)           # second-difference matrix
+    # DTD = D.T @ D
+
+    # w = np.ones(L)
+
+    # for _ in range(niter):
+    #     W = np.diag(w)
+    #     # solve (W + lam * D^T D) z = W y
+    #     Z = np.linalg.solve(W + lam * DTD, w * y_med)
+    #     w = asym * (y_med > Z) + (1 - asym) * (y_med < Z)
+
+    # baseline = Z
+    # df_proc["BASELINE"] = baseline
+    # df_proc["INTENSITY_CORR"] = y_med - baseline
 
     snv = (df_proc["INTENSITY_CORR"] - df_proc["INTENSITY_CORR"].mean()) / df_proc["INTENSITY_CORR"].std()
     df_proc["INTENSITY_SNV"] = snv
@@ -71,7 +96,7 @@ def process_spectrum(filename, log_callback=print):
         log_callback(f"Processed spectrum saved as: {stem}_processed.csv")
 
     # --- Library matching ---
-    library_df = pd.read_csv(LIB_DIR / "lib.csv", header=None)
+    library_df = pd.read_csv(LIB_DIR / "lib.csv", header=None) 
     material_names = [library_df.iloc[0, col] for col in range(1, library_df.shape[1], 2)]
 
     best_match = None
